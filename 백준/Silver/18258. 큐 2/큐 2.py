6e619f7f42fc984815a65f9e345b455db9e7b4e8
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2026-01-12 18:31:29 +0900

    [Silver IV] Title: 큐 2, Time: 1444 ms, Memory: 144228 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+N = int(input())
+q = deque()
+
+for _ in range(N) :
+    c = input().split()
+    
+    if c[0] == "push" :
+        q.append(c[1])
+    elif c[0] == "pop" :
+        if len(q) > 0 :
+            print(q[0])
+            q.popleft()
+        else :
+            print(-1)
+    elif c[0] == "size" :
+        print(len(q))
+    elif c[0] =="empty" :
+        if len(q) == 0 :
+            print(1)
+        else :
+            print(0)
+    elif c[0] == "front" :
+        if len(q) > 0 :
+            print(q[0])
+        else :
+            print(-1)
+    elif c[0] == "back" :
+        if len(q) > 0 :
+            print(q[-1])
+        else :
+            print(-1)
