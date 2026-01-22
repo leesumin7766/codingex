c27c1c8e59a1b6eb51626b5133045f3f8c08e456
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2026-01-23 02:48:00 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,10 @@
+def solution(rsp):
+    answer = ''
+    for s in rsp :
+        if s == "2" :
+            answer += "0"
+        elif s == "0" :
+            answer += "5"
+        else :
+            answer += "2"
+    return answer
